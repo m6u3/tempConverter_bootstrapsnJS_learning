be9ab2a734fcd7c8808b3e29a87473c823f2af16
m6u3 <mgarl22@gmail.com>
2021-08-21 20:02:35 -0400

    half way with JS elemnt manipulation

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 function celToFah(){
+    //Creating formula to Convert Temps
     let temp = document.getElementById("temp").value;
     let tempInF = (temp * 9/5) + 32;
-
+    //alert( temp + "° Celsius is " + tempInF + "° Fahrenheit.")
+    //Creating <p> elemnt and text inside, appending the results.
+    let pText = document.createTextNode(temp + "° Celsius is " + tempInF + "° Fahrenheit.");
+    let para = document.createElement("p");
+    para.appendChild(pText);
+    //Creating <buttto> to reset page
+    let button = document.createElement("button");
+    let resetText = document.createTextNode("Reset");
+    button.appendChild(resetText);
+    //Appending new elements and their contents to <div id="input-result"
     let inputResult = document.getElementById("input-result");
-    inputResult.innerHTML = temp + "° Celsius is " + tempInF + "° Fahrenheit";
+    inputResult.innerHTML =  "";
+    inputResult.appendChild(para);
+    inputResult.appendChild(button);
+    //adding atributes for class and auto bootstraps style. for button
+    let primaryButton = document.createAttribute("class");
+    primaryButton.value = "btn btn-primary";
+    button.setAttributeNode(primaryButton);
+    //adding onclick event to button.
+    let onclick = document.createAttribute("onclick");
+    onclick.value = "reset()";
+    button.setAttributeNode(onclick);
 
 }
 function fahToCel(){
@@ -11,4 +31,8 @@ function fahToCel(){
     let tempInC = (temp - 32) * 5/9;
     tempInC = Math.round(tempInC * 100) / 100;
     alert(temp + "° Fahrenheit is " + tempInC + "° Celsius");
+}
+
+function reset(){
+    alert("you are resetting");
 }
